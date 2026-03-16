b9af22adab5f205655b420cf0e195461f61aed77
yszm213 <zmedyanis@gmail.com>
2026-03-16 16:35:53 +0100

    Update consigne.c

--- consigne.c
+++ consigne.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include "consigne.h"
 
-   float consigne(float thermostatPrec_f)
-   {
-    float thermostat_f = thermostatPrec_f;
+   float consigne(float thermostatPrec_f){
 
-    FILE* verrou = fopen(".verrouConsigne","r");
-    if (verrou != NULL){
-        fclose(verrou);
-        return thermostat_f;
-    }
+      float thermostat_f = thermostatPrec_f;
 
-    verrou = fopen(".verrouConsigne","w");
-    if (verrou == NULL){
-        return thermostat_f;
-    }
-    fclose(verrou);
+      FILE* verrou = fopen(".verrouConsigne","r");
+      if (verrou != NULL){
+         fclose(verrou);
+         return thermostat_f;
+      }
 
-    FILE* fichier = fopen("consigne.txt","r");
-    if (fichier != NULL){
-        if (fscan(fichier,"%f",&thermostat_f) != 1) {
+      verrou = fopen(".verrouConsigne","w");
+      if (verrou == NULL){
+         return thermostat_f;
+      }
+      fclose(verrou);
+
+      FILE* fichier = fopen("consigne.txt","r");
+      if (fichier != NULL){
+         if (fscanf(fichier,"%f",&thermostat_f) != 1) {
             thermostat_f = thermostatPrec_f;
-        }
-        fclose(fichier);
-    }
-    remove(".verrouConsigne");
-    return thermostat_f;
+         }
+         fclose(fichier);
+      }
+      remove(".verrouConsigne");
+      return thermostat_f;
    }
    
    
