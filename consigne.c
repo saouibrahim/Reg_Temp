1b85642e249846484afd81834860b6d1add8c2d3
yszm213 <zmedyanis@gmail.com>
2026-03-16 16:33:31 +0100

    correction

--- consigne.c
+++ consigne.c
@@ -1,11 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "consigne.h"
 
    float consigne(float thermostatPrec_f)
    {
-   
-      float thermostat_f=0;
-   
-     
-      return thermostat_f;
-   
+    float thermostat_f = thermostatPrec_f;
+
+    FILE* verrou = fopen(".verrouConsigne","r");
+    if (verrou != NULL){
+        fclose(verrou);
+        return thermostat_f;
+    }
+
+    verrou = fopen(".verrouConsigne","w");
+    if (verrou == NULL){
+        return thermostat_f;
+    }
+    fclose(verrou);
+
+    FILE* fichier = fopen("consigne.txt","r");
+    if (fichier != NULL){
+        if (fscan(fichier,"%f",&thermostat_f) != 1) {
+            thermostat_f = thermostatPrec_f;
+        }
+        fclose(fichier);
+    }
+    remove(".verrouConsigne");
+    return thermostat_f;
    }
+   
+   
--- include/consigne.h
+++ include/consigne.h
@@ -2,29 +2,4 @@
 #include <stdlib.h>
 #include "define.h" 
 
-float consigne(float thermostatPrec_f) {
-
-    float thermostat_f = thermostatPrec_f;
-
-    FILE* verrou = fopen(".verrouConsigne","r");
-    if (verrou != NULL){
-        fclose(verrou);
-        return thermostat_f;
-    }
-
-    verrou = fopen(".verrouConsigne","w");
-    if (verrou == NULL){
-        return thermostat_f;
-    }
-    fclose(verrou);
-
-    FILE* fichier = fopen("consigne.txt","r");
-    if (fichier != NULL){
-        if (fscan(fichier,"%f",&thermostat_f) != 1) {
-            thermostat_f = thermostatPrec_f;
-        }
-        fclose(fichier);
-    }
-    remove(".verrouConsigne");
-    return thermostat_f;
-}
+float consigne(float thermostatPrec_f);
