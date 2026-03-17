25746070d4b67a00d5e3e88f01450fbc1dd4fbdb
yszm213 <zmedyanis@gmail.com>
2026-03-17 09:31:38 +0100

    Update regulation.c

--- regulation.c
+++ regulation.c
@@ -1,9 +1,16 @@
     #include <stdio.h>
-	 #include <stdlib.h>
+	#include <stdlib.h>
     #include "regulation.h"
+	#include "define.h"
 	 
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
-		float cmd;
+		float cmd = 0;
+
+		if (tabT == NULL){
+			printf("Aucune valeur de température dans le tableau\n");
+			return cmd;
+		}
+		
 		
 		return cmd;
 	}
