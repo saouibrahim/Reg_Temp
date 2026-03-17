09a4e0ca353f91aef5a66e427e9fb9edc444cb26
saouibrahim <ibrsaou@gmail.com>
2026-03-17 11:09:23 +0100

    visualisationT a moitie fini

--- visualisationT.c
+++ visualisationT.c
@@ -1,6 +1,18 @@
 #include "visualisationT.h"
 
-void visualisationT(temp_t myTemp)
+FILE *file;
+
+void visualisationT(temp_t maTemperature)
 {
-   
+    if (access(".verrouData", F_OK) == 0)
+    {
+        file = fopen(".verrourData", "w"); // cest pour dire que le fichier data.txt est en cours d'utilisation
+        fclose(file);
+
+        file = fopen("data.txt", "w");
+
+        fprintf(file, "%.2f ");
+
+        fclose(file);
+    }
 }
