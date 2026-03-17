ae9115bc4809d89c840b01e0ed3dd41f0011c877
yszm213 <zmedyanis@gmail.com>
2026-03-17 09:54:33 +0100

    Update regulation.c

--- regulation.c
+++ regulation.c
@@ -6,12 +6,26 @@
 	float regulationTest(int regul,float consigne,float* tabT, int nT){
 		float cmd = 0;
 
-		if (tabT == NULL){
+		if (nT <= 0 || tabT == NULL){ // pas de température et tableau de température vide
 			printf("Aucune valeur de température dans le tableau\n");
 			return cmd;
 		}
 		
-		
+		// MODE 1 : TOUT OU RIEN
+		if (regul ==1){
+			if (tabT[nT-1] < consigne){ // si température précédente < consigne
+				cmd = 50; // on chauffe 
+			}
+			else{ // si température > consigne 
+				cmd = 0; // on chauffe pas 
+			}
+		}
+
+		// MODE 2 : PID
+		else if (regul == 2){
+			static float erreur_prec = 0.0;
+		}
+
 		return cmd;
 	}
     
