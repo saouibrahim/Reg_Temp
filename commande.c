d2527a98c52e42b432f1bc04d4b26f659dc73ce9
yszm213 <zmedyanis@gmail.com>
2026-03-18 10:37:09 +0100

    Update commande.c

--- commande.c
+++ commande.c
@@ -15,7 +15,7 @@ void commande(float puissance, FT_HANDLE ftHandle){
     unsigned char PUIS = (unsigned char)((puissance*127)*100);
     unsigned char octet_commande = PUIS;
 
-    // Envoi via USB et vérif du résultat
+    // Envoi via USB et vérif du résultat, dispo sur la documentation de la bibliothèque FTD2XX
     DWORD BytesWritten;
     FT_STATUS status = FT_Write(ftHandle, &octet_commande, 1, &BytesWritten);
     if (status != FT_OK || BytesWritten != 1) {
