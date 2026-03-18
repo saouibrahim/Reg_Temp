241859f7d8b65cbcf094def6573a410a02881996
yszm213 <zmedyanis@gmail.com>
2026-03-18 09:09:03 +0100

    reg

---
+++ include/commande.h
@@ -0,0 +1,10 @@
+#ifndef COMMANDE_H
+#define COMMANDE_H
+
+#include <windows.h>
+#include <stdio.h>
+#include "ftd2xx.h"
+
+void commande(float puissance, FT_HANDLE ftHandle);
+
+#endif
--- regulation.c
+++ regulation.c
@@ -1,21 +1,17 @@
 #include "regulation.h"
 
-static float regulation_1ou0(float csgn, float temperature_int)
-{
+static float regulation_1ou0(float csgn, float temperature_int){
 	float cmd = 0;
-	if (temperature_int < csgn)
-	{
+	if (temperature_int < csgn){
 		cmd = 50;
 	}
-	else
-	{
+	else{
 		cmd = 0;
 	}
 	return cmd;
 }
 
-static float regulation_PID(float csgn, float temperature_int, float *erreur_prec, float *int_prec, bool *isFirst)
-{
+static float regulation_PID(float csgn, float temperature_int, float *erreur_prec, float *int_prec, bool *isFirst){
 	float intermedI;
 	float intermedD;
 
@@ -24,7 +20,8 @@ static float regulation_PID(float csgn, float temperature_int, float *erreur_pre
 	if (*isFirst) {
 		intermedI = 0;
 		intermedD = 0;
-	} else {
+	} 
+	else {
 		intermedI = *int_prec + ((erreur + *erreur_prec) / 2.0f) * dt;
 		intermedD = (erreur - *erreur_prec) / dt;
 	}
@@ -35,12 +32,10 @@ static float regulation_PID(float csgn, float temperature_int, float *erreur_pre
 
 	float cmd = P + I + D;
 
-	if (cmd > 100)
-	{
+	if (cmd > 100){
 		cmd = 100;
 	}
-	else if (cmd < 0)
-	{
+	else if (cmd < 0){
 		cmd = 0;
 	}
 
@@ -49,35 +44,30 @@ static float regulation_PID(float csgn, float temperature_int, float *erreur_pre
 	return cmd;
 }
 
-float regulationTest(int regul, float csgn, float *tabT, int nT)
-{
+float regulationTest(int regul, float csgn, float *tabT, int nT){
 	float cmd = 0;
 	float err = 0;
 	float int_avant = 0;
 	bool isFirst;
 
-	if (nT <= 0 || tabT == NULL)
-	{
+	if (nT <= 0 || tabT == NULL){
 		printf("Aucune valeur de température dans le tableau\n");
 		return cmd;
 	}
 
 	// MODE 1 : TOUT OU RIEN
-	if (regul == 1)
-	{
+	if (regul == 1){
 		cmd = regulation_1ou0(csgn, tabT[nT - 1]);
 	}
 	// MODE 2 : PID
-	else if (regul == 2)
-	{
+	else if (regul == 2){
 		isFirst = true;
 		for (int i = 0; i < nT; i++) {
 			cmd = regulation_PID(csgn, tabT[i], &err, &int_avant, &isFirst);
 			isFirst = false;
 		}
 	}
-	else
-	{
+	else{
 		printf("Erreur ! Veuillez selectionner 1 pour un mode de chauffage tout ou rien ou 2 pour le mode de chauffage par PID.\n");
 	}
 
