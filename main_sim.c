2a23dba13dcc67759bb6b527ad323d5b259b1f1a
yszm213 <zmedyanis@gmail.com>
2026-03-18 11:36:47 +0100

    Update main_sim.c

--- main_sim.c
+++ main_sim.c
@@ -17,14 +17,16 @@ int main() {
     int nT = 0; // nb de temp enregistrées
     float tabT[10000]; // tab des temps
 
+    // création et init du simu 
     struct simParam_s* monSimulateur = simConstruct(maTemp);
 
     printf("Démarrage de la simulation...\n");
 
     while (1) {
+        // lecture du fichier consigne.txt avec l'appel de 'consigne.h'
         maConsigne = consigne(maConsigne);
 
-        // si la temp est inf à 5deg
+        // si la temp est inf à 5deg, on break 
         if (maConsigne <= 5.0) {
             commande = 0.0;
             visualisationC(commande);
@@ -32,15 +34,19 @@ int main() {
             break;
         }
 
+        // enregistre la temp int actuelle dans le tableau
         if (nT < 10000) {
             tabT[nT] = maTemp.interieure;
             nT++;
         }
 
+        // calcul de la puis de chauffe via regul == (1 ou 2)
         commande = regulationTest(regul, maConsigne, tabT, nT);
+        // maj de la temp via "simulateur.h"
         maTemp = simCalc(commande, monSimulateur);
-
+        // affiche temp int et ext
         visualisationT(maTemp);
+        // affiche puis de chauffe 
         visualisationC(commande);
 
         sleep(1);
