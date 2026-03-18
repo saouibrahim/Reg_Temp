fad269fd001b8719238bb9454cfacd8970051b72
yszm213 <zmedyanis@gmail.com>
2026-03-18 08:41:36 +0100

    correct

--- include/regulation.h
+++ include/regulation.h
@@ -2,7 +2,8 @@
 #define REGULATION_H
 
 #include <stdio.h>         
-#include <stdlib.h>        
+#include <stdlib.h> 
+#include <stdbool.h>
 #include "define.h"        // Accès à KP, KI, KD, dt, temp_t
 
 float regulationTest(int regul, float csgn, float* tabT, int nT);  
--- regulation.c
+++ regulation.c
@@ -1,46 +1,40 @@
 #include "regulation.h"
 
-
 static float regulation_1ou0(float csgn, float temperature_int)
 {
 	float cmd = 0;
-
 	if (temperature_int < csgn)
-	{			  // si température précédente < consigne
-		cmd = 50; // on chauffe
+	{
+		cmd = 50;
 	}
 	else
-	{			 // si température > consigne
-		cmd = 0; // on chauffe pas
+	{
+		cmd = 0;
 	}
-
 	return cmd;
 }
 
 static float regulation_PID(float csgn, float temperature_int, float *erreur_prec, float *int_prec, bool *isFirst)
 {
-    float intermedI;
+	float intermedI;
 	float intermedD;
-    
-	float erreur = csgn - temperature_int; // erreur actuelle
-    
-    if(*isFirst){
-	    intermedI = 0;
-	    intermedD = 0;
-	} 
-	else {
-	    intermedI = *int_prec + ((((erreur - *erreur_prec)/dt) * 2.0) + (*erreur_prec * dt));
-	    intermedD = ((erreur - *erreur_prec) / dt);
+
+	float erreur = csgn - temperature_int;
+
+	if (*isFirst) {
+		intermedI = 0;
+		intermedD = 0;
+	} else {
+		intermedI = *int_prec + ((erreur + *erreur_prec) / 2.0f) * dt;
+		intermedD = (erreur - *erreur_prec) / dt;
 	}
-    
-	float P = KP * erreur;	  // proportionnel
-	float I = KI * intermedI; // intégral
-	float D = KD * intermedD; // dérivé
-	
+
+	float P = KP * erreur;
+	float I = KI * intermedI;
+	float D = KD * intermedD;
 
 	float cmd = P + I + D;
 
-	// sécurité : si cmd ne se situe plus entre 0 et 100
 	if (cmd > 100)
 	{
 		cmd = 100;
@@ -50,24 +44,20 @@ static float regulation_PID(float csgn, float temperature_int, float *erreur_pre
 		cmd = 0;
 	}
 
-	*erreur_prec = erreur; // l'erreur actuelle devient l'erreur précédent pour le prochain calcul
+	*erreur_prec = erreur;
 	*int_prec = intermedI;
-
 	return cmd;
 }
 
-
 float regulationTest(int regul, float csgn, float *tabT, int nT)
 {
-
 	float cmd = 0;
 	float err = 0;
 	float int_avant = 0;
 	bool isFirst;
-	
 
 	if (nT <= 0 || tabT == NULL)
-	{ // pas de température et tableau de température vide
+	{
 		printf("Aucune valeur de température dans le tableau\n");
 		return cmd;
 	}
@@ -77,20 +67,15 @@ float regulationTest(int regul, float csgn, float *tabT, int nT)
 	{
 		cmd = regulation_1ou0(csgn, tabT[nT - 1]);
 	}
-
 	// MODE 2 : PID
 	else if (regul == 2)
 	{
-	    for(int i=0; i<nT; i++){
-	        if (i == 0){
-	            isFirst = true;
-	            cmd = regulation_PID(csgn, tabT[i], &err, &int_avant, &isFirst);
-	        }
-	        isFirst = false;
-	        cmd = regulation_PID(csgn, tabT[i], &err, &int_avant, &isFirst);
-	    }
+		isFirst = true;
+		for (int i = 0; i < nT; i++) {
+			cmd = regulation_PID(csgn, tabT[i], &err, &int_avant, &isFirst);
+			isFirst = false;
+		}
 	}
-
 	else
 	{
 		printf("Erreur ! Veuillez selectionner 1 pour un mode de chauffage tout ou rien ou 2 pour le mode de chauffage par PID.\n");
@@ -98,5 +83,3 @@ float regulationTest(int regul, float csgn, float *tabT, int nT)
 
 	return cmd;
 }
-
-
